302d9753e9135e60fae5afe03ab13c2ef1d02ee0
Steve Trap <trap.steve@hotmail.com>
2018-10-29 14:09:52 +0100

    changed colors for layout

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -6,10 +6,10 @@ export default class AboutScreen extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue', alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={styles.pageTitle}>About</Text>
+        <View style={{flex: 1, backgroundColor: '#734b74', alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={styles.pageTitle}>Add Todo</Text>
         </View>
-        <View style={{flex: 5, backgroundColor: 'skyblue'}} >
+        <View style={{flex: 5, backgroundColor: '#dccddc'}} >
         <ScrollView>
           <Text style={styles.text}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget est vestibulum nisi lobortis sagittis id vitae magna. Aliquam convallis, ex vitae mattis condimentum, dolor enim dapibus urna, nec convallis tellus magna vel lorem. Nulla sit amet orci eu odio auctor commodo. Donec quis arcu euismod, ultricies ex eu, tincidunt metus. Quisque vulputate erat eget maximus gravida. Duis lectus ipsum, iaculis vel justo eget, mattis bibendum velit. Donec in tincidunt elit. Fusce tincidunt bibendum ligula, egestas vestibulum metus bibendum eu.
@@ -28,6 +28,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 30,
     paddingTop: 15,
+    color: '#dccddc',
   },
   text: {
     fontSize: 20,
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,19 +6,18 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue', alignItems: 'center', justifyContent: 'center'}}>
+        <View style={{flex: 1, backgroundColor: '#734b74', alignItems: 'center', justifyContent: 'center'}}>
         <Text style={styles.pageTitle}>Add Todo</Text>
         </View>
-        <View style={{flex: 5, backgroundColor: 'skyblue'}} >
+        <View style={{flex: 5, backgroundColor: '#dccddc'}} >
 
         <View style ={styles.btnContainer}>
-        <Button 
+        <Button style ={styles.button}
           onPress={() => {
             Alert.alert('Adding Todo!');
           }}
           title="+"
         /></View>
-
         </View>
       </View>
     );
@@ -30,8 +29,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 30,
     paddingTop: 15,
+    color: '#dccddc',
   },
   btnContainer: {
+    
     width: 40,  
     height: 40,                                                   
     position: 'absolute',                                          
