055f353051185892c8ecbe7631103970dc4c63e2
Steve Trap <trap.steve@hotmail.com>
2018-10-29 10:33:29 +0100

    Basic layout with stylesheet, still need fix for code duplication

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation'; // Version can be specified in package.json
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to About"
-          onPress={() => {
-            this.props.navigation.dispatch(StackActions.reset({
-              index: 0,
-              actions: [
-                NavigationActions.navigate({ routeName: 'About' })
-              ],
-            }))
-          }}
-        />
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue', alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={styles.pageTitle}>Add Todo</Text>
+        </View>
+        <View style={{flex: 4, backgroundColor: 'skyblue'}} />
       </View>
     );
   }  
 }
+
+const styles = StyleSheet.create({
+  pageTitle: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    paddingTop: 10,
+  },
+});
