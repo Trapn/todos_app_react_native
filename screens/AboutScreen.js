d0f471ff68198d047c94e78c42ddee4ab948f407
Steve Trap <trap.steve@hotmail.com>
2018-11-19 15:11:25 +0100

    added basic auth with token and using AsyncStorage

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView, AsyncStorage } from 'react-native';
 
 export default class AboutScreen extends React.Component {
+  _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('auth_token');
+      if (value !== null) {
+        // We have data!!
+        console.log(value);
+      }
+    } catch (error) {
+      // Error retrieving data
+    }
+  };
   render() {
     return (
         <View style={styles.pageView}>
@@ -10,6 +21,10 @@ export default class AboutScreen extends React.Component {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget est vestibulum nisi lobortis sagittis id vitae magna. Aliquam convallis, ex vitae mattis condimentum, dolor enim dapibus urna, nec convallis tellus magna vel lorem. Nulla sit amet orci eu odio auctor commodo. Donec quis arcu euismod, ultricies ex eu, tincidunt metus. Quisque vulputate erat eget maximus gravida. Duis lectus ipsum, iaculis vel justo eget, mattis bibendum velit. Donec in tincidunt elit. Fusce tincidunt bibendum ligula, egestas vestibulum metus bibendum eu.
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare urna elit, ut commodo ex tincidunt nec. Aliquam semper, nisl id tempor hendrerit, ligula tortor tincidunt nulla, at congue nibh ipsum in risus. Mauris consectetur erat felis, quis volutpat erat ultrices nec. Nullam rhoncus eros tempor, malesuada nibh non, consectetur nunc. Integer quam enim, consectetur et elit eget, pretium condimentum metus. Quisque commodo lorem ac nisl mollis faucibus. Aenean odio ligula, lacinia a finibus et, pellentesque et felis. Morbi tristique lacinia finibus. Aenean aliquam pulvinar est, ac viverra ante efficitur eu. Phasellus bibendum tellus in elit egestas sollicitudin. 
           </Text>
+          <Button
+            onPress={this._retrieveData.bind(this)}
+            title="GetToken"
+          />
         </ScrollView>
         </View>
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,60 +1,129 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Alert, Form, TextInput , InputField} from 'react-native';
 
-export default class HomeScreen extends React.Component {
+import {
+  AsyncStorage,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+  Alert,
+} from 'react-native';
+ 
+var STORAGE_KEY = 'auth_token';
+
+export default class HomeScreen extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {username: '',
+                  email: ''};
+  };
+    async _onValueChange(item, selectedValue) {
+    try {
+      await AsyncStorage.setItem(item, selectedValue);
+    } catch (error) {
+      console.log('AsyncStorage error: ' + error.message);
+    }
+};
+
+_retrieveData = async () => {
+  try {
+    const value = await AsyncStorage.getItem(STORAGE_KEY);
+    if (value !== null) {
+      // We have data!!
+      console.log(value);
+    }
+  } catch (error) {
+    // Error retrieving data
   }
+};
+
+  async _userLogout() {
+    try {
+      await AsyncStorage.removeItem(STORAGE_KEY);
+      Alert.alert("Logout Success!")
+    } catch (error) {
+      console.log('AsyncStorage error: ' + error.message);
+    }
+  };
+  _userLogin() { 
+      fetch("http://vives-todoapp.herokuapp.com/authenticate", {
+        method: "POST", 
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: this.state.email, 
+          password: this.state.password, 
+        })
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        this._onValueChange(STORAGE_KEY, responseData.auth_token)
+        console.log(responseData)
+        console.log(this.state.email)
+        console.log(responseData.auth_token)
+        
+      })
+      .done();
+    }; 
+
   render() {
     return (
-      <View style={{paddingTop: 25}}>
+      <ScrollView style={{padding: 20}}>
+      <Text 
+          style={{fontSize: 27}}>
+          Login
+      </Text>
+      <TextInput placeholder='Username' 
+      value={this.state.email} 
+      onChangeText={(text) => this.setState({ email: text })} />
+      <TextInput placeholder='Password' 
+      value={this.state.password} 
+      onChangeText={(text) => this.setState({ password: text })} />
+      <View style={{margin:7}} />
+      <Button
+            onPress={this._userLogin.bind(this)}
+            title="Login"
+          />
+            <Button
+            onPress={this._retrieveData.bind(this)}
+            title="GetToken"
+          />
+      </ScrollView>
 
-      <View style={{padding: 10}}>
-      <TextInput
-          style={{height: 40}}
-          placeholder="Project Title"
-          onChangeText={(text) => this.setState({text})}
-        />
-
-        <Button 
-          onPress={() => {
-            fetch('https://vives-todoapp.herokuapp.com/projects', {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              title: this.state.text,
-              description: 'Added hardcoded from addbutton',
-              user_id: 1,
-            }),
-            
-          });
-          Alert.alert('Adding Todo!');
-          }}
-          title="+"
-        />
-      </View>
-    </View>
     );
-  }  
-}
+  } 
 
-const styles = StyleSheet.create({
-  pageTitle: {
-    fontWeight: 'bold',
-    fontSize: 30,
-    paddingTop: 15,
-    color: '#dccddc',
+}
+ 
+var styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
-  btnContainer: {
-    
-    width: 40,  
-    height: 40,                                                   
-    position: 'absolute',                                          
-    bottom: 10,                                                    
-    right: 10,  
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   },
 });
